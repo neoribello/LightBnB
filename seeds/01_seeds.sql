16f3743df6631b62bfe2e2807153d8513107f31c
Neil Oribello <neil.oribello@gmail.com>
2021-03-03 22:09:44 +0000

    editing the TABLE reservations

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -9,7 +9,7 @@ VALUES (1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/208667
 (2, 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&amp;cs=tinysrgb&amp;h=350', ' https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland', '44583', true);
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
-VALUES (2018-09-11, 2018-09-26, 2, 3),
+VALUES ('2018-09-11', '2018-09-26', 2, 3),
 ('2019-01-04', '2019-02-01', 2, 2),
 ('2021-10-01', '2021-10-04', 1, 4);
 
